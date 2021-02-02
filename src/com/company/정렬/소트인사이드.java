e0f6291732747683a6c8541301e93340ddb8e309
jelly-boop <jellyjelly111@naver.com>
2021-02-02 20:34:33 +0900

    19일차

---
+++ src/com/company/정렬/소트인사이드.java
@@ -0,0 +1,17 @@
+package com.company.정렬;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class 소트인사이드 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String a=scanner.nextLine();
+        String[]arr=a.split("");
+        Arrays.sort(arr, Collections.reverseOrder());
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]);
+        }
+    }
+}
---
+++ src/com/company/정렬/좌표정렬하기.java
@@ -0,0 +1,7 @@
+package com.company.정렬;
+
+public class 좌표정렬하기 {
+    public static void main(String[] args) {
+
+    }
+}
