3e81f640d901d3ab0809dede437c39ef2f33d1c3
jelly-boop <jellyjelly111@naver.com>
2021-06-29 23:40:22 +0900

    DFS BFS

--- src/com/company/DFS와BFS/DFS와BFS.java
+++ src/com/company/DFS와BFS/DFS와BFS.java
@@ -1,5 +1,8 @@
 package com.company.DFS와BFS;
 
+
+import javafx.event.EventDispatchChain;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,24 +12,21 @@ import java.util.StringTokenizer;
 
 public class DFS와BFS {
     static int[][]arr;
-    static  boolean[] check;
-    static StringBuilder sb=new StringBuilder();
-
+    static boolean[]check;
+    private static StringBuilder sb=new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st=new StringTokenizer(br.readLine()," ");
-        int n=Integer.parseInt(st.nextToken());
-        int m=Integer.parseInt(st.nextToken());
-        int v=Integer.parseInt(st.nextToken());
-
+        int n=Integer.parseInt(st.nextToken());//정점의 개수
+        int m=Integer.parseInt(st.nextToken());//간선의 개수
+        int v=Integer.parseInt(st.nextToken());//시작번호
         arr=new int[n+1][n+1];
         check=new boolean[n+1];
-
-        //노드,간선 초기화
-        for(int i=0;i<m;i++){
-            st= new StringTokenizer(br.readLine());
+        while (m-->0){
+            st=new StringTokenizer(br.readLine()," ");
             int s=Integer.parseInt(st.nextToken());
             int e=Integer.parseInt(st.nextToken());
+            //선 연결
             arr[s][e]=1;
             arr[e][s]=1;
         }
@@ -35,21 +35,24 @@ public class DFS와BFS {
         bfs(v);
         System.out.println(sb);
 
+
     }
 
-    private static void initCheck() {
-        for(int i=0;i< check.length;i++){
-            check[i]=false;
+    private static void dfs(int v) {
+        sb.append(v+" ");
+        check[v]=true;// 방문햇다고 체크
+        for(int i=1;i<check.length;i++){
+            if(i!=v&&check[i]==false&&arr[v][i]==1){
+                dfs(i);
+            }
         }
     }
-
-    private static void bfs(int start) {
-        //check배열 초기화
+    private static void bfs(int v) {
         initCheck();
-        Queue<Integer>queue=new LinkedList<>();
+        Queue<Integer> queue=new LinkedList<>();
         //시작노드 추가
-        queue.add(start);
-        check[start]=true;
+        queue.add(v);
+        check[v]=true;
         while (!queue.isEmpty()){
             int n=queue.poll();
             sb.append(n+" ");
@@ -59,21 +62,15 @@ public class DFS와BFS {
                     check[i]=true;
                 }
             }
-
         }
 
     }
 
-
-    private static void dfs(int start) {
-        //경로
-        sb.append(start+" ");
-        check[start]=true;
-
-        for(int i=1;i< check.length;i++){
-            //현재 노드와 다른 노드중 미방문&간선 존재
-            if(i!=start&&check[i]==false&&arr[start][i]==1)
-                dfs(i);
+    private static void initCheck() {
+        for(int i=0;i< check.length;i++){
+            check[i]=false;
         }
     }
+
+
 }
