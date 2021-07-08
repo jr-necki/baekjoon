7b2a7199af0ba94f73c4ad36482b5529eb6b4d22
jelly-boop <jellyjelly111@naver.com>
2021-07-08 19:27:47 +0900

    이분탐색

---
+++ src/com/company/이분탐색/수찾기_1920.java
@@ -0,0 +1,18 @@
+package com.company.이분탐색;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class 수찾기_1920 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> a=new ArrayList<>();
+        ArrayList<Integer> a2=new ArrayList<>();
+
+        StringTokenizer st=new StringTokenizer(br.readLine()," ");
+
+    }
+}
