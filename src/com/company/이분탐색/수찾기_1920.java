3fbf5eac52c9321070be21c2f58d5aafe94b5fd8
jelly-boop <jellyjelly111@naver.com>
2021-07-09 10:54:35 +0900

    이분탐색

--- src/com/company/이분탐색/수찾기_1920.java
+++ src/com/company/이분탐색/수찾기_1920.java
@@ -4,15 +4,48 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class 수찾기_1920 {
     public static void main(String[] args) throws IOException {
+        ArrayList<Integer> al=new ArrayList<>();
+
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<Integer> a=new ArrayList<>();
-        ArrayList<Integer> a2=new ArrayList<>();
-
+        int n=Integer.parseInt(br.readLine());
         StringTokenizer st=new StringTokenizer(br.readLine()," ");
+        while (n-->0){
+            al.add(Integer.parseInt(st.nextToken()));
+        }
+        Collections.sort(al);
+        int n2=Integer.parseInt(br.readLine());
+        StringTokenizer st2=new StringTokenizer(br.readLine()," ");
+        while (n2-->0){
 
+            int target=Integer.parseInt(st2.nextToken());
+            System.out.println(binarySearch(al,target));
+        }
     }
+
+    private static int binarySearch(ArrayList<Integer> al, int target) {
+        int first=0;
+        int last=al.size()-1;
+        int mid=0;
+
+        while (first<=last){
+            mid=(first+last)/2;
+            if(target==al.get(mid)){
+                return 1;
+            }else{
+                if(target<al.get(mid)){
+                    last=mid-1;
+                }else{
+                    first=mid+1;
+                }
+            }
+        }
+        return 0;
+    }
+
+
 }
