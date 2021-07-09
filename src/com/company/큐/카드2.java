3c7d3f06a27fe5fd6fc89e160351b346bb8a651a
jelly-boop <jellyjelly111@naver.com>
2021-07-09 10:54:14 +0900

    큐

---
+++ src/com/company/큐/카드2.java
@@ -0,0 +1,28 @@
+package com.company.큐;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class 카드2 {
+    public static void main(String[] args) throws IOException {
+        Queue<Integer> queue=new LinkedList<>();
+
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int cards=Integer.parseInt(br.readLine());
+        int i=1;
+        //카드 세팅팅
+       while (cards-->0){
+            queue.add(i);
+            i++;
+        }
+       while (queue.size()!=1){
+           queue.poll();
+           int card=queue.poll();
+           queue.add(card);
+       }
+       System.out.println(queue.peek());
+    }
+}
